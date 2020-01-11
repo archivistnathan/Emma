f3ef87e7daae5dbdcaa2e9a9e30aa6a199649a25
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-01-11 08:17:17 +0800

    Update illum_debug.py

--- sensor_debug/illum_debug.py
+++ sensor_debug/illum_debug.py
@@ -17,7 +17,7 @@ import time
 
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 # get visible illuminance from BH1750 using I2C 0x23
-# code from https://bitbucket.org/MattHawkinsUK/rpispy-misc/raw/master/python/bh1750.py
+# code derived from https://bitbucket.org/MattHawkinsUK/rpispy-misc/raw/master/python/bh1750.py and http://www.pibits.net/code/raspberry-pi-bh1750-light-sensor.php
 # MAKE SURE TO ENABLE I2C IN RPi-config
 
 # Define some constants from the datasheet
@@ -40,4 +40,35 @@ def readLight(addr=BHSEN):
  
 print "Light Level : " + str(readLight()) + " lux"
 
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+# get visible illuminance from TSL2561 using I2C 0x39
+# code derived from https://github.com/ControlEverythingCommunity/TSL2561/blob/master/Python/TSL2561.py
+# MAKE SURE TO ENABLE I2C IN RPi-config
 
+# TSL2561 address, 0x39(57)
+# Select control register, 0x00(00) with command register, 0x80(128)
+#		0x03(03)	Power ON mode
+bus.write_byte_data(0x39, 0x00 | 0x80, 0x03)
+# TSL2561 address, 0x39(57)
+# Select timing register, 0x01(01) with command register, 0x80(128)
+#		0x02(02)	Nominal integration time = 402ms
+bus.write_byte_data(0x39, 0x01 | 0x80, 0x02)
+
+time.sleep(0.5)
+
+# Read data back from 0x0C(12) with command register, 0x80(128), 2 bytes
+# ch0 LSB, ch0 MSB
+data = bus.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
+
+# Read data back from 0x0E(14) with command register, 0x80(128), 2 bytes
+# ch1 LSB, ch1 MSB
+data1 = bus.read_i2c_block_data(0x39, 0x0E | 0x80, 2)
+
+# Convert the data
+ch0 = data[1] * 256 + data[0]
+ch1 = data1[1] * 256 + data1[0]
+
+# Output data to screen
+print "Full Spectrum(IR + Visible) :%d lux" %ch0
+print "Infrared Value :%d lux" %ch1
+print "Visible Value :%d lux" %(ch0 - ch1)
