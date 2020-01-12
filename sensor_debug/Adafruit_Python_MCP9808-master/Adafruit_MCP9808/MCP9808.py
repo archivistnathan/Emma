0ef739c95db71c7aceb76f10ac6249eedd802b41
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-01-12 17:45:35 +0800

    Update MCP9808.py

--- sensor_debug/Adafruit_Python_MCP9808-master/Adafruit_MCP9808/MCP9808.py
+++ sensor_debug/Adafruit_Python_MCP9808-master/Adafruit_MCP9808/MCP9808.py
@@ -80,7 +80,7 @@ class MCP9808(object):
 		# Read temperature register value.
 		t = self._device.readU16BE(MCP9808_REG_AMBIENT_TEMP)
 		bytet = bytes(16)
-		bytet = bytes(self._device.readU16BE(MCP9808_REG_AMBIENT_TEMP))
+		bytet = self._device.readU16BE(MCP9808_REG_AMBIENT_TEMP)
 		print str(bin(bytet))[2:]
 		self._logger.debug('Raw ambient temp register value: 0x{0:04X}'.format(t & 0xFFFF))
 		# Scale and convert to signed value.
