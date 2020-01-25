5731ff4cbb1b5506d9ba1d51269fc55649832cd1
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-01-25 12:51:14 +0800

    User sensor comparison testcodes

--- sensor_debug/mcw-test.py
+++ sensor_debug/mcw-test.py
@@ -2,23 +2,23 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-PIR_PIN=14
-GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+MCW_PIN=14
+GPIO.setup(MCW_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 usercount = 0
 
-def MOTION(PIR_PIN):
-	print "Motion Detected"
+def MOTION(MCW_PIN):
+	print "Motion Detected by Microwave Sensor"
 	global usercount
 	usercount = usercount + 1
 	print "Current Usercount",usercount
 
-print "PIR Module Test"
+print "Microwave Module Test"
 time.sleep(2)
 print "Ready"
 
 try:
-	GPIO.add_event_detect(PIR_PIN,GPIO.RISING,callback=MOTION)
+	GPIO.add_event_detect(MCW_PIN,GPIO.RISING,callback=MOTION)
 	while 1:
 		time.sleep(100)
 except KeyboardInterrupt:
---
+++ sensor_debug/user-test.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+MCW_PIN=14
+
+GPIO.setup(MCW_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PIRM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+mcwusercount = 0
+pirusercount = 0
+pirmusercount = 0
+
+def MCWMOTION(MCW_PIN):
+	print "Motion Detected by Microwave Sensor"
+	global mcwusercount
+	mcwusercount = mcwusercount + 1
+	print "Current Usercount",mcwusercount
+
+def PIRMOTION(PIR_PIN):
+	print "Motion Detected by PIR Sensor"
+	global pirusercount
+	pirusercount = pirusercount + 1
+	print "Current Usercount",pirusercount
+
+def PIRMMOTION(PIRM_PIN):
+	print "Motion Detected by Modified PIR Sensor"
+	global pirmusercount
+	pirmusercount = pirmusercount + 1
+	print "Current Usercount",pirmusercount
+
+print "User Count Module Test"
+time.sleep(2)
+print "Ready"
+
+try:
+	GPIO.add_event_detect(MCW_PIN,GPIO.RISING,callback=MCWMOTION)
+	GPIO.add_event_detect(PIR_PIN,GPIO.RISING,callback=PIRMOTION)
+	GPIO.add_event_detect(PIRM_PIN,GPIO.RISING,callback=PIRMOTION)
+	while 1:
+		time.sleep(100)
+except KeyboardInterrupt:
+	print "Quit."
+	print " Final microwave user count: ",mcwusercount
+	print " Final PIR user count: ",pirusercount
+	print " Final modified PIR user count: ",pirmusercount
+	GPIO.cleanup()
