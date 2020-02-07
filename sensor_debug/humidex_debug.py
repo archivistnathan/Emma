75a5f0e38394cddb5e8996dbf1012acfd2203351
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-02-07 20:53:16 +0800

    Update humidex_debug.py

--- sensor_debug/humidex_debug.py
+++ sensor_debug/humidex_debug.py
@@ -149,6 +149,28 @@ if __name__ == '__main__':
 	print "Temperature from HTU21D is    : %.2f C" %htutemp
 	print "Humidity from HTU21D is    : %.2f RH" %htuhum
 
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+# get SHT31 temperature and humidity using I2C 0x44
+# code from http://www.pibits.net/code/raspberry-pi-sht31-sensor-example.php
+
+# SHT31 address, 0x44(68), bus already declared in MCP9808 section
+bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+ 
+time.sleep(0.5)
+ 
+# SHT31 address, 0x44(68)
+# Read data back from 0x00(00), 6 bytes
+# Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+data = bus.read_i2c_block_data(0x44, 0x00, 6)
+ 
+# Convert the data
+temp = data[0] * 256 + data[1]
+cTemp = -45 + (175 * temp / 65535.0)
+humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+ 
+# Output data to screen
+print "Temperature in Celsius is : %.3f C" %cTemp
+print "Relative Humidity is : %.2f %%RH" %humidity
 
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 # validate readings by redundant comparison
