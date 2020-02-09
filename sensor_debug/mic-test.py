46908d126655272d08b422f384db09e43d834622
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-02-09 19:37:50 +0800

    Update mic-test.py

--- sensor_debug/mic-test.py
+++ sensor_debug/mic-test.py
@@ -17,7 +17,7 @@
 # import libraries for GPIO and I2C
 import subprocess
 
-monitor = subprocess.run(["arecord", "-D", "plughw:1,0", "-qd", "1", "monitor.wav"])
+monitor = subprocess.run(["arecord", "-D", "plughw:1,0", "-qd", "1", "monitor.wav"], shell=TRUE)
 print('Monitor State:',monitor.returncode)
 
 process = subprocess.Popen(["sox", "monitor.wav", "-n", "stats"], 
