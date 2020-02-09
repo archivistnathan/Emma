22f81e629787ba8741597342b93b45313815d053
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-02-09 17:49:13 +0800

    Create mic-test.py

---
+++ sensor_debug/mic-test.py
@@ -0,0 +1,35 @@
+# ------------------------------------------------------------------------------
+# Environmental Monitoring and Management for Archives (EMMA) Project
+#
+# Test functionality of microphone used as sound intensity sensor
+#
+# PRIVACY NOTICE
+# This code captures audio clips with a 1s duration. The clip is then automatically processed by the sox utility to return statistics relating to sound intensity. The clip is overwritten on the next reading cycle.
+#
+# FOR DEBUGGING ONLY
+#
+# (c) 2020 Jonathan Isip, Quezon City, Philippines
+# A project with the University of the Philippines School of Library and Information Studies
+# Released under GNU General Public License (GPL v3.0) 
+# email nathan@slis.upd.edu.ph
+# ------------------------------------------------------------------------------
+
+# import libraries for GPIO and I2C
+import os
+
+process = subprocess.Popen(['arecord', '-D plughw:1,0 -qd 1 test1.wav && sox test1.wav -n stats
+'], 
+                           stdout=subprocess.PIPE,
+                           universal_newlines=True)
+
+while True:
+    output = process.stdout.readline()
+    print(output.strip())
+    # Do something else
+    return_code = process.poll()
+    if return_code is not None:
+        print('RETURN CODE', return_code)
+        # Process has finished, read rest of the output 
+        for output in process.stdout.readlines():
+            print(output.strip())
+        break
