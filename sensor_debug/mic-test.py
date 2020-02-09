f58c8ce3f6a39cc71228186330a3b61f240ed1c1
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-02-09 18:01:01 +0800

    Update mic-test.py

--- sensor_debug/mic-test.py
+++ sensor_debug/mic-test.py
@@ -17,7 +17,7 @@
 # import libraries for GPIO and I2C
 import subprocess
 
-process = subprocess.Popen(['arecord', '-D plughw:1,0 -qd 1 test1.wav'], 
+process = subprocess.Popen(['sox', 'test1.wav -n stats'], 
                            stdout=subprocess.PIPE,
                            universal_newlines=True)
 
