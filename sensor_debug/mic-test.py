91c946c1b815b350c7c9893f0347aaaf3f5d1868
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-02-10 20:25:53 +0800

    Update mic-test.py

--- sensor_debug/mic-test.py
+++ sensor_debug/mic-test.py
@@ -24,12 +24,15 @@ import csv
 subprocess.call(["arecord", "-D", "plughw:1,0", "-qd", "1", "monitor.wav"])
 
 # Use sox to get clip stats
-process = subprocess.Popen(["sox", "monitor.wav", "-n", "stats"], 
-                           stdout=subprocess.PIPE,
-                           universal_newlines=True)
-soundblock = process.communicate()[0]
+soundblock = subprocess.check_output(["sox", "monitor.wav", "-n", "stats"])
+print(soundblock)
 
-# print(soundblock)
+#process = subprocess.Popen(["sox", "monitor.wav", "-n", "stats"], 
+#                           stdout=subprocess.PIPE,
+#                           universal_newlines=True)
+#soundblock = process.communicate()[0]
+
+# 	print(soundblock)
 
 #while True:
 #	output = process.stdout.readline()
