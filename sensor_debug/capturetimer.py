392b346b7145cd3a2c4970ca43934bac4dfcef70
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-02-12 15:23:40 +0800

    Update capturetimer.py

--- sensor_debug/capturetimer.py
+++ sensor_debug/capturetimer.py
@@ -5,12 +5,12 @@ from datetime import timedelta
 tl = Timeloop()
 
 execfile('humidex_debug.py')
-print("Initial humidex capture ",time,time())
+print("Initial humidex capture ",time.time())
 
 @tl.job(interval=timedelta(seconds=30))
 def humidex_capture():
 	execfile('humidex_debug.py')
-	print("Humidex captured ",time,time())
+	print("Humidex captured ",time.time())
 
 if __name__ == "__main__":
     tl.start(block=True)
