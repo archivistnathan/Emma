f1eee4ae4a5f80d6f386eae69648918ba6c8735f
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-02-12 15:22:31 +0800

    Update capturetimer.py

--- sensor_debug/capturetimer.py
+++ sensor_debug/capturetimer.py
@@ -9,7 +9,7 @@ print("Initial humidex capture ",time,time())
 
 @tl.job(interval=timedelta(seconds=30))
 def humidex_capture():
-	execfile('humidex.py')
+	execfile('humidex_debug.py')
 	print("Humidex captured ",time,time())
 
 if __name__ == "__main__":
