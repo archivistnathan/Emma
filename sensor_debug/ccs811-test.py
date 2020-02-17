1291cefa7589f37c53829428d08dff7820a2ebcb
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-02-17 22:35:59 +0800

    Update ccs811-test.py

--- sensor_debug/ccs811-test.py
+++ sensor_debug/ccs811-test.py
@@ -26,6 +26,9 @@ CCS811_ERROR_ID = 0xE0
 CCS811_APP_START = 0xF4
 CCS811_SW_RESET = 0xFF
 
+value = bus.read_i2c_block_data(CCS811_ADDR, CCS811_STATUS, 1)
+print (value[0] << 3)
+
 bus.write_i2c_block_data(CCS811_ADDR, CCS811_MEAS_MODE, [0x10])
 
 value = bus.read_i2c_block_data(CCS811_ADDR, CCS811_STATUS, 1)
