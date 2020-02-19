13b9652b04cd59bcf0498d39915bb30d49d7bfcb
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-02-19 12:41:15 +0800

    Update capture_illuminance.py

--- capture_illuminance.py
+++ capture_illuminance.py
@@ -37,7 +37,7 @@ def convertToNumber(data):
  
 def readLight(addr=BHSEN):
   bus.write_byte(addr,POWER_ON)
-  time.sleep(0.5)
+  time.sleep(1)
   data = bus.read_i2c_block_data(addr,ONE_TIME_HIGH_RES_MODE)
   return convertToNumber(data)
  
