6a14ba283a564e8fbd80b67c9682784a693b994c
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-02-19 09:00:05 +0800

    Update capture_illum.py

--- capture_illum.py
+++ capture_illum.py
@@ -37,6 +37,7 @@ def convertToNumber(data):
  
 def readLight(addr=BHSEN):
   bus.write_byte(addr,POWER_ON)
+  time.sleep(0.5)
   data = bus.read_i2c_block_data(addr,ONE_TIME_HIGH_RES_MODE)
   return convertToNumber(data)
  
