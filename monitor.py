3f7b3f168af406983a3532a8bb2fd667866add26
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-03-22 12:56:24 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -11,3 +11,22 @@
 
 # import configuration file and required libraries
 import config
+import time
+from timeloop import Timeloop
+from datetime import timedelta
+
+# This code uses the Timedelta library to schedule when readings are captured
+# https://medium.com/greedygame-engineering/an-elegant-way-to-run-periodic-tasks-in-python-61b7c477b679
+
+tl = Timeloop()
+
+execfile('capture_humidex.py')
+print("Humidex capture started",time.time())
+
+@tl.job(interval=timedelta(minutes=10))
+def humidex_capture():
+	execfile('capture_humidex.py')
+	print("Humidex captured ",time.time())
+
+if __name__ == "__main__":
+    tl.start(block=True)
