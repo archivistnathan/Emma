c5388c48e27cf9d39f80b4204c1b84371c8c3b4f
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-03-23 11:29:18 +0800

    Update capture_gaslevel.py

--- capture_gaslevel.py
+++ capture_gaslevel.py
@@ -4,7 +4,18 @@
 
 from configure_gaslevel import *
 import time, datetime
+import threading
 
+# Calibrate sensor and determine Ro
 mq = MQ();
-perc = mq.MQPercentage()
-print("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
+
+# Read data
+def readgaslevel():
+	while 1:
+		perc = mq.MQPercentage()
+		print("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
+		time.sleep(0.1)
+print "Gas level monitoring started"
+
+thread = threading.Thread(target=readgaslevel)
+thread.start()
