a0de7bb92c68ccfb7af624b31719911a9f8bc42b
Nathan <56992436+archivistnathan@users.noreply.github.com>
2020-03-23 08:29:08 +0800

    Update capture_usercount.py

--- capture_usercount.py
+++ capture_usercount.py
@@ -1,18 +1,25 @@
 import RPi.GPIO as GPIO
-import time
+import time, datetime
+import config
 
 GPIO.setmode(GPIO.BCM)
 MCW_PIN=14
 
 GPIO.setup(MCW_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-mcwusercount = 0
-
 def MCWMOTION(MCW_PIN):
 	print "Motion Detected by Microwave Sensor"
-	global mcwusercount
-	mcwusercount = mcwusercount + 1
-	print "Current Microwave Usercount",mcwusercount
+
+	htimestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+	insertval = (htimestamp,config.sensor_id)
+	insertquery = "INSERT INTO usercount (tstamp, sensorid) VALUES (%s, %s)",insertval
+
+	cursor = config.dbconnect.cursor()
+	cursor.execute(*insertquery)	
+	config.dbconnect.commit()
+	print(cursor.rowcount, "Record succesfully inserted into usercount table")
+	cursor.close()
 
 print "User Count Module Test"
 time.sleep(2)
